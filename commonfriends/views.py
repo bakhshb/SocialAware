bac11b18234fc77499127e8b6b96fdcacd393455
Baraa <bakhshb@gmail.com>
2016-08-08 04:35:41 +1200

    writing code to check the received bluetooth is already the logged in user friend, or if the received does not have any friends at all

--- commonfriends/views.py
+++ commonfriends/views.py
@@ -25,12 +25,16 @@ class UserBluetooth(APIView):
 	def post(self, request):
 		logger.info("Register User Bluetooth")
 		data = request.data
-		bluetooth = data.get('user_bluetooth','')
+		user_bluetooth = data.get('user_bluetooth','')
+		# Checking if user_bluetooth is submitted
+		if user_bluetooth == '':
+			logger.info("User Bluetooth is empty")
+			return Response(status = status.HTTP_204_NO_CONTENT, data={'status': status.HTTP_204_NO_CONTENT})
 
 		user = request.user
 		onto = OntologyManager(user)
 		onto_user = onto.get_user()
-		onto.create_bluetooth(bluetooth)
+		onto.create_bluetooth(user_bluetooth)
 		logger.debug(onto_user.has_bluetooth)
 		onto.save_ontology()
 
@@ -45,29 +49,66 @@ class SearchFriendByBluetooth(APIView):
 	def dispatch(self, *args, **kwargs):
 		return super(SearchFriendByBluetooth, self).dispatch(*args, **kwargs)
 	def post(self, request):
+		# Friend status
+		NO_MATCH_FOUND = 0
+		USER_ALREADY_FRIEND =1
+		FOUND_MUTUAL_FRIENDS = 2
+
 		logger.info("Receiving Surrounding Bluetooth")
 		data = request.data
 		bluetooth = data.get('bluetooth','')
+		# Checking if bluetooth is submitted
+		if bluetooth == '':
+			logger.info("Bluetooth is empty")
+			return Response(status = status.HTTP_204_NO_CONTENT, data={'status': status.HTTP_204_NO_CONTENT})
+
 
 		# Finding the user by bluetooth address in the ontology
 		onto_received = OntologyManager()
 		onto_received_user = onto_received.get_user_by_bluetooth(bluetooth)
 		if onto_received_user is None:
 			logger.debug("No Match Found")
-			return Response(status = status.HTTP_204_NO_CONTENT, data={'status': status.HTTP_204_NO_CONTENT})
-		
+			data = {
+				'status':'204',
+				'friend_status': NO_MATCH_FOUND,
+				'friend': [],
+			}
+			return Response(status = status.HTTP_204_NO_CONTENT, data= data)
+		# if user is found, then get user friend list
 		onto_received_user_friends = onto_received.get_friends_name()
 
 		# Getting the current user detail from ontology
 		user = request.user
 		onto_current = OntologyManager(user)
 		onto_current_user_friends = onto_current.get_friends_name()
+		# Checking if the received bluetooth is already friend with the current user
+		if parsing_to_str(onto_received_user.has_name) in onto_current_user_friends:
+			logger.info("User is already your friend")
+			data = {
+				'status':'200',
+				'user': parsing_to_str(onto_received_user.has_name),
+				'friend_status': USER_ALREADY_FRIEND,
+				'friend': [],
+			}
+			return Response(status = status.HTTP_200_OK,data=data)
+
+
 		# Comapring both friend list and finding mutual friends
 		mutual = set(onto_current_user_friends) & set(onto_received_user_friends)
+		# No mutual friend found
+		if not mutual:
+			data = {
+				'status':'204',
+				'friend_status': NO_MATCH_FOUND,
+				'friend': [],
+			}
+			return Response(status = status.HTTP_204_NO_CONTENT, data= data)
+
 		data = {
 		'status':'200',
 		'user': parsing_to_str(onto_received_user.has_name),
-		'friend': mutual
+		'friend_status': FOUND_MUTUAL_FRIENDS,
+		'friend': mutual,
 		}
 		return Response(status = status.HTTP_200_OK,data=data)
 
